4fbe74e474a54266bbf3e3fcf420a7f034597c2d
Karox1234 <zzzcomet@gmail.com>
2024-02-14 15:56:03 +0900

    Feat : 초기 설정 업로드

---
+++ src/main/kotlin/com/teamsparta/hotelcaching/HotelCachingApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.hotelcaching
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelCachingApplication
+
+fun main(args: Array<String>) {
+    runApplication<HotelCachingApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/hotelcaching/HotelCachingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.hotelcaching
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelCachingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
