24de0980359206a532cbe0f4f17f9b485efa0090
Karox1234 <zzzcomet@gmail.com>
2024-02-14 21:21:20 +0900

    Feat : create,delete 추가

--- src/main/kotlin/com/teamsparta/hotelcaching/domain/hotel/controller/HotelController.kt
+++ src/main/kotlin/com/teamsparta/hotelcaching/domain/hotel/controller/HotelController.kt
@@ -1,12 +1,16 @@
 package com.teamsparta.hotelcaching.domain.hotel.controller
 
+import com.teamsparta.hotelcaching.domain.hotel.dto.HotelRequest
 import com.teamsparta.hotelcaching.domain.hotel.dto.HotelResponse
 import com.teamsparta.hotelcaching.domain.hotel.service.HotelService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -29,5 +33,17 @@ class HotelController(
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(hotelService.getHotelList(page, size))
+    }
+
+    @PostMapping
+    fun createHotel(request: HotelRequest):ResponseEntity<HotelResponse> {
+        val createHotel = hotelService.createHotel(request)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createHotel)
+    }
+
+    @DeleteMapping("/{hotelId}")
+    fun deleteHotel(@PathVariable hotelId: Long):ResponseEntity<Unit> {
+        hotelService.deleteHotel(hotelId)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
--- src/main/kotlin/com/teamsparta/hotelcaching/domain/hotel/service/HotelServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/hotelcaching/domain/hotel/service/HotelServiceImpl.kt
@@ -1,8 +1,12 @@
 package com.teamsparta.hotelcaching.domain.hotel.service
 
+import com.teamsparta.hotelcaching.domain.hotel.dto.HotelRequest
 import com.teamsparta.hotelcaching.domain.hotel.dto.HotelResponse
+import com.teamsparta.hotelcaching.domain.hotel.model.HotelEntity
 import com.teamsparta.hotelcaching.domain.hotel.model.toResponse
 import com.teamsparta.hotelcaching.domain.hotel.repository.HotelRepository
+import com.teamsparta.hotelcaching.exception.ModelNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,5 +18,21 @@ class HotelServiceImpl(
     @Transactional(readOnly = true)
     override fun getHotelList(page: Int, size: Int): List<HotelResponse> {
         return hotelRepository.findAll().map { it.toResponse() }
+    }
+
+    override fun createHotel(request: HotelRequest): HotelResponse {
+        val hotel = HotelEntity(
+            content = request.content,
+            grade = 0.0,
+            name = request.name,
+            price = request.price
+        )
+        val saveHotel = hotelRepository.save(hotel)
+        return saveHotel.toResponse()
+    }
+
+    override fun deleteHotel(hotelId: Long) {
+        val hotel = hotelRepository.findByIdOrNull(hotelId) ?: throw ModelNotFoundException("Hotel",hotelId)
+        hotelRepository.delete(hotel)
     }
 }
